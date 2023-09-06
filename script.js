76f40db6a4d02d6dddf8d1708cc88d9549acaf81
Sayf Sahaf <sayf.sahaf@gmail.com>
2023-09-06 02:01:21 -0400

    finish basic functionality. Data fetch works

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // api that fetches a quote
-
-async function fetchData() {
+async function fetchQuote() {
     const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
     const options = {
       method: 'GET',
@@ -8,20 +7,45 @@ async function fetchData() {
         'X-RapidAPI-Key': '068e53c32fmsh49505e5411e1a01p16ca76jsnd0a08a30547e',
         'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
       }
-    };
+    }
   
     try {
       const response = await fetch(url, options);
       const result = await response.json();
-      document.getElementById("quote").innerText = result.content;
+      console.log(result)
+      document.getElementById("quote").innerHTML = '<p>' + result.content + '</p>';
+      document.getElementById("qAuthor").innerHTML = '<p>- ' + result.originator.name + '</p>';
+      if (result.originator.description == "") {
+        
+      } 
+      else {
+
+      }
     } catch (error) {
       console.error(error);
     }
 }
 
-// shows a quote and the author of the quote when the website first loads up
-//fetchData();
 
-document.getElementById("quote").innerText = 'This is random text for testing purposes. This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes. This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.This is random text for testing purposes.'
-document.getElementById("qAuthor").innerText = '- Neil Alexander Armstrong'
+
+//shows a quote and the author of the quote when the website first loads up
+fetchQuote();
+
+
+//getting the buttons
+const copyB = document.getElementById('btnC')
+const AboutB = document.getElementById('btnA')
+const NextB = document.getElementById('btnN')
+
+NextB.addEventListener("click", function() {
+    fetchQuote();
+})
+
+
+
+
+
+
+
+
   
