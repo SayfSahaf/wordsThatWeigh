ce2bbf85eb482583cdf94f83ec09ff7053888ef1
Sayf Sahaf <sayf.sahaf@gmail.com>
2023-09-06 18:38:41 -0400

    fix about button bug that did not show the description even when available

--- script.js
+++ script.js
@@ -12,17 +12,15 @@ async function fetchQuote() {
     try {
       const response = await fetch(url, options);
       result = await response.json();
-      console.log(result)
       document.getElementById("quote").innerHTML = '<p>' + result.content + '</p>';
       document.getElementById("qAuthor").innerHTML = '<p>- ' + result.originator.name + '</p>';
       if (result.originator.description == "") {
         AboutB.classList.remove('canClick');
         AboutB.classList.add('noClick')
       }
-      if (result.originator.description == !"") {
+      else if (result.originator.description != "") {
         AboutB.classList.remove('noClick')
         AboutB.classList.add('canClick')
-        
       }
     } catch (error) {
       console.error(error);
@@ -35,8 +33,7 @@ async function fetchQuote() {
 fetchQuote();
 
 
-//getting the buttons
-
+//getting the buttons and elements & initializing variables
 let result;
 const copyB = document.getElementById('btnC')
 const AboutB = document.getElementById('btnA')
@@ -44,22 +41,21 @@ const NextB = document.getElementById('btnN')
 const description = document.getElementById('description')
 let isToggled = false;
 
+//event listener to fetch a new quote
 NextB.addEventListener("click", function() {
     fetchQuote();
 })
 
-if (AboutB.classList.contains('canClick')) {
-    AboutB.addEventListener('click', function() {
-        isToggled = !isToggled
+AboutB.addEventListener('click', function() {
+    isToggled = !isToggled
+    if (isToggled) {
+        description.innerHTML = '<p>' + result.originator.description + '</p>'
+    }
+    else {
+        description.innerHTML = ''
+    }
+})
 
-        if (isToggled) {
-            description.innerHTML = '<p>' + result.originator.description + '</p>'
-        }
-        if (!isToggled) {
-            description.innerHTML = ''
-        }
-    })
-}
 
 
 
